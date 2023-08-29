0343938c0f6f8a7ed0d1d406a62fe9794ea33614
charmed <cutqqq@mail.ru>
2023-08-29 15:13:06 +0500

    комит32

---
+++ hwjs1/jscode.js
@@ -0,0 +1,9 @@
+// 1.Задача
+const celsius = 15;
+let formulaFarengeit = ((9 / 5) * celsius + 32);
+console.log(` температура по цельсию ${celsius} и температура  по Фаренгейту ${formulaFarengeit}`);
+
+// 2 задача
+let name = "Stas";
+let admin = name;
+console.log(`Значение второй задачи ${admin}`);
